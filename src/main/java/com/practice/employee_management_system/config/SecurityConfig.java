4dff4720d07921d8c125953ef069767a3356187e
zaina-r <zaienam-se21040@stu.kln.ac.lk>
2025-05-09 23:31:15 +0530

    Add Spring Auth

---
+++ src/main/java/com/practice/employee_management_system/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.practice.employee_management_system.config;
+
+import com.practice.employee_management_system.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        //builder pattern
+        //todo: check how this code works and more notes on builder pattern and lambda expressions
+        return httpSecurity
+                .csrf(Customizer -> Customizer.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/employee/all")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder(10));
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        return authenticationProvider;
+
+    }
+}
---
+++ src/main/java/com/practice/employee_management_system/model/EmployeePrincipal.java
@@ -0,0 +1,56 @@
+package com.practice.employee_management_system.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class EmployeePrincipal implements UserDetails {
+
+    private Employee employee;
+
+    public EmployeePrincipal(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return employee.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return employee.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+//        return UserDetails.super.isAccountNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+//        return UserDetails.super.isAccountNonLocked();
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+//        return UserDetails.super.isCredentialsNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+//        return UserDetails.super.isEnabled();
+        return true;
+    }
+}
