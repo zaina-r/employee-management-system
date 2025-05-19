10e524a7b75a645af98ef8d6203a883c8239a968
zaina-r <zaienam-se21040@stu.kln.ac.lk>
2025-05-19 22:16:25 +0530

    Enable react router

--- frontend/employee-management-system/src/main.jsx
+++ frontend/employee-management-system/src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import Home from './home.jsx'
+import AllEmployees from './Employees.jsx'
+import Login from './Login.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+        <Route exact path='/employee/login' element={<Login />} />
+        <Route exact path='/employee/all' element={<AllEmployees/>}/>
+      </Routes>
+    </Router>
   </StrictMode>,
 )
