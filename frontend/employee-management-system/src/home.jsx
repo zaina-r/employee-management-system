cf4c373f2c0ce53ff66ebdf1025028cb86a21d3a
zaina-r <zaienam-se21040@stu.kln.ac.lk>
2025-05-20 23:25:08 +0530

    Clean up code

--- frontend/employee-management-system/src/home.jsx
+++ frontend/employee-management-system/src/home.jsx
@@ -1,18 +1,24 @@
-import './Home.css'
+import "./Home.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-      <>
-          <div className='login employee-login'>
-              <p>Login as employee</p>
-              <button className='employee-login login-button'>Login</button>
+    <>
+      <div className="container-m text-center">
+        <div className="row m-5">
+          <div className="col login m-3 p-5 border">
+            <p>Login as employee</p>
+            <Link type="button" className="btn btn-primary btn-lg employee-login" to='/employee/login'>Employee Login</Link>
           </div>
-          <div className='login admin-login'>
-              <p>Log in as Admin</p>
-              <button className='admin-login login-button'>Login</button>
+          <div className="col login m-3 p-5 border">
+            <p>Log in as Admin</p>
+            <button type="button" className="btn btn-primary btn-lg employee-login">Admin Login</button>
           </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
