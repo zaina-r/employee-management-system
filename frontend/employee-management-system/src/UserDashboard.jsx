6bbe2e8bbd67f9aab438fa348ebde9f8e8845f6f
zaina-r <zaienam-se21040@stu.kln.ac.lk>
2025-06-03 23:02:27 +0530

    Create User dashboard page

---
+++ frontend/employee-management-system/src/UserDashboard.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
+function UserDashboard({ username }) {
+  const [employee, setEmployee] = useState("");
+
+  useEffect(() => {
+    loadEmployee();
+  }, []);
+
+  const loadEmployee = async () => {
+    try {
+      const token = localStorage.getItem("JWT");
+      const decoded = jwtDecode(token);
+      const employeeId = decoded.employeeId;
+
+      const response = await axios.get("http://localhost:8080/employee/all", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setEmployee(response.data);
+    } catch (error) {
+      if (error.response) {
+        // Server responded with a status other than 2xx
+        console.error(
+          "Backend error:",
+          error.response.status,
+          error.response.data
+        );
+        alert(
+          `Error: ${error.response.status} - ${
+            error.response.data.message || error.message
+          }`
+        );
+      } else if (error.request) {
+        // Request made but no response
+        console.error("No response from server", error.request);
+        alert("No response from server. Check backend and CORS.");
+      } else {
+        // Something else happened
+        console.error("Error", error.message);
+        alert("Error: " + error.message);
+      }
+    }
+  };
+
+    return (<>
+    
+        <div className="container-fluid">
+            <h1>Welcome, {employee.firstName}</h1>
+            <p>EmployeeId: {employee.employeeId} | {employee.designation}</p>
+            
+        </div>
+    </>)
+}
+
+export default UserDashboard;
